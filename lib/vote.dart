03510e94b2e3a68e517c46fd6aa097b1d54a52c0
William Costa <wd.costa@campus.fct.unl.pt>
2020-11-29 20:38:45 +0000

    Voting is working successfully

--- lib/vote.dart
+++ lib/vote.dart
@@ -11,8 +11,8 @@ class Vote extends StatefulWidget {
 class _VoteState extends State<Vote> {
   final _formKey = GlobalKey<FormState>();
 
-  List<String> names = ["Gata", "Gatinha", "Gato"];
-  List<String> votingOptions = ["Branco", "A favor"];
+  List names = [];
+  List votingOptions = [];
   int _value = 0;
   int _votingValue = 0;
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -20,79 +20,87 @@ class _VoteState extends State<Vote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          title: Text("Vote"),
-        ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    DropdownButton(
-                      value: _value,
-                      onChanged: (value) {
-                        setState(() {
-                          _value = value;
-                        });
-                      },
-                      items: names
-                          .map((e) => DropdownMenuItem(
-                                value: names.indexOf(e),
-                                child: Text(e),
-                              ))
-                          .toList(),
-                    ),
-                    SizedBox(
-                      width: 50,
-                    ),
-                    DropdownButton(
-                      value: _votingValue,
-                      onChanged: (value) {
-                        setState(() {
-                          _votingValue = value;
-                        });
-                      },
-                      items: votingOptions
-                          .map((e) => DropdownMenuItem(
-                                value: votingOptions.indexOf(e),
-                                child: Text(e),
-                              ))
-                          .toList(),
-                    ),
-                  ],
-                ),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  _scaffoldKey.currentState.showSnackBar(new SnackBar(
-                    content: new Text('Voto foi enviado com sucesso!'),
-                  ));
-                },
-                child: Text("Enviar Voto"),
-              ),
-              Flexible(
-                child: StreamBuilder(
-                  stream: Firestore.instance.collection('vote').snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) return Text("No data Found");
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text("Vote"),
+      ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('vote_options').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Text("No data Found");
 
-                    return ListView.builder(
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, index) => ListTile(
-                        title: Text(snapshot.data.documents[index]['name']),
-                        subtitle: Text(snapshot.data.documents[index]['full']),
+          names = snapshot.data.documents[0]['names'];
+          votingOptions = snapshot.data.documents[0]['options'];
+
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DropdownButton(
+                        value: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value = value;
+                          });
+                        },
+                        items: names
+                            .map((e) => DropdownMenuItem(
+                                  value: names.indexOf(e),
+                                  child: Text(e),
+                                ))
+                            .toList(),
                       ),
-                    );
-                  },
+                      SizedBox(
+                        width: 50,
+                      ),
+                      DropdownButton(
+                        value: _votingValue,
+                        onChanged: (value) {
+                          setState(() {
+                            _votingValue = value;
+                          });
+                        },
+                        items: votingOptions
+                            .map((e) => DropdownMenuItem(
+                                  value: votingOptions.indexOf(e),
+                                  child: Text(e),
+                                ))
+                            .toList(),
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
-          ),
-        ));
+                RaisedButton(
+                  onPressed: () {
+                    var vote = {'vote': votingOptions[_votingValue]};
+                    var voteCollection =
+                        FirebaseFirestore.instance.collection('vote');
+                    voteCollection.doc(names[_value]).get().then((snapshot) {
+                      if (snapshot.exists) {
+                        _scaffoldKey.currentState.showSnackBar(
+                            new SnackBar(content: new Text('Já votaste!')));
+                      } else {
+                        snapshot.reference.set(vote).then((value) =>
+                            _scaffoldKey.currentState.showSnackBar(new SnackBar(
+                              content:
+                                  new Text('Voto foi enviado com sucesso!'),
+                            )));
+                      }
+                    });
+                  },
+                  child: Text("Enviar Voto"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
