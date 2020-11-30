7815e88bc33ce5b51d2e71173a3f682a7a907633
William Costa <wd.costa@campus.fct.unl.pt>
2020-11-30 23:28:05 +0000

    Can change priority of fingers

--- lib/Admin/fingers.dart
+++ lib/Admin/fingers.dart
@@ -15,6 +15,7 @@ class _FingersState extends State<Fingers> {
   Map<String, List<String>> idList;
   List<String> priorityList;
   List<String> documentIds;
+  TextEditingController _fingerPriority = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +35,13 @@ class _FingersState extends State<Fingers> {
             documentIds = [];
             fullList = {};
             idList = {};
+            _fingerPriority.text = "";
 
             for (String priority in prioritySnapshot.data.documents[0]['priority']) {
               fullList.putIfAbsent(priority, () => new List<String>());
               idList.putIfAbsent(priority, () => new List<String>());
               priorityList.add(priority);
+              _fingerPriority.text += priority + ", ";
             }
 
             for (var document in snapshot.data.documents) {
@@ -54,24 +57,55 @@ class _FingersState extends State<Fingers> {
             print(names);
             print(fingers);
 
-            return ListView.builder(
-              itemCount: names.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  hoverColor: Colors.grey[60],
-                  title: Text(names[index]),
-                  subtitle: Text(fingers[index]),
-                  trailing: IconButton(
-                    icon: Icon(Icons.check),
-                    onPressed: () {
-                      FirebaseFirestore.instance.collection('finger').doc(documentIds[index]).delete();
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: names.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        hoverColor: Colors.grey[60],
+                        title: Text(names[index]),
+                        subtitle: Text(fingers[index]),
+                        trailing: IconButton(
+                          icon: Icon(Icons.check),
+                          onPressed: () {
+                            FirebaseFirestore.instance.collection('finger').doc(documentIds[index]).delete();
+                          },
+                        ),
+                        onTap: () {
+                          FirebaseFirestore.instance.collection('finger').doc(documentIds[index]).delete();
+                        },
+                      );
                     },
                   ),
-                  onTap: () {
-                    FirebaseFirestore.instance.collection('finger').doc(documentIds[index]).delete();
-                  },
-                );
-              },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: _fingerPriority,
+                          decoration: InputDecoration(
+                              labelText: 'Dedos por prioridade (Maior prioridade para Menor Prioridade)', hintText: 'Separar por vírgulas'),
+                        ),
+                      ),
+                      RaisedButton(
+                        child: Text('Mudar Prioridades'),
+                        onPressed: () {
+                          var priorityTemp = _fingerPriority.text.split(', ');
+                          priorityTemp.remove('');
+                          FirebaseFirestore.instance
+                              .collection('finger_priority')
+                              .doc(prioritySnapshot.data.documents[0].documentID)
+                              .set({'priority': priorityTemp.map((e) => e.contains(',') ? e.substring(0, e.length - 1) : e)});
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
             );
           },
         );
